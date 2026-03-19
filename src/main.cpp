7a3945c652ceb6f6ed26b18826460d92f577ff0a
jv <jv.bronzeado@hotmail.com>
2026-03-19 18:18:19 -0300

    project setup

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Vem dançar forró, vem\n");
+    printf("Vem dançar forró, vem\n");
+    printf("Vem dançar forró deixa os problemas pra depois\n");
+    return 0;
+}
