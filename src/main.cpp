969f3eaba281a6a2104b77ebcca673b366573c7a
jv <jv.bronzeado@hotmail.com>
2026-03-19 18:46:58 -0300

    gurobi config

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,51 @@
 #include <stdio.h>
+#include <gurobi_c++.h>
+
+using namespace std;
 
 int main(int argc, char** argv) {
     printf("Vem dançar forró, vem\n");
     printf("Vem dançar forró, vem\n");
     printf("Vem dançar forró deixa os problemas pra depois\n");
+
+    try {
+        // Formulate and solve model
+        
+        // Create an environment
+        GRBEnv env = GRBEnv(true);
+        env.set("LogFile", "mip1.log");
+        env.start();
+
+        GRBModel model = GRBModel(env);
+
+        // Create variables
+        GRBVar x = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x");
+        GRBVar y = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "y");
+        GRBVar z = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "z");
+
+        // Add constraint: x + 2 y + 3 z <= 4
+        model.addConstr(x + 2 * y + 3 * z <= 4, "c0");
+
+        // Set objective: maximize x + y + 2 z
+        model.setObjective(x + y + 2 * z, GRB_MAXIMIZE);
+
+        // Optimize model
+        model.optimize();
+
+        cout << x.get(GRB_StringAttr_VarName) << " "
+             << x.get(GRB_DoubleAttr_X) << endl;
+        cout << y.get(GRB_StringAttr_VarName) << " "
+             << y.get(GRB_DoubleAttr_X) << endl;
+        cout << z.get(GRB_StringAttr_VarName) << " "
+             << z.get(GRB_DoubleAttr_X) << endl;
+
+        cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+    } catch(GRBException e) {
+        cout << "Error code = " << e.getErrorCode() << endl;
+        cout << e.getMessage() << endl;
+    } catch(...) {
+        cout << "Exception during optimization" << endl;
+    }
+    
     return 0;
 }
